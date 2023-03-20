42e1523f9db32d207a4cbe53f34623d254027aae
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-19 18:21:21 -0700

    fixed code got annotations to work

--- CSE335Lab5/DetailView.swift
+++ CSE335Lab5/DetailView.swift
@@ -18,102 +18,76 @@ struct Location: Identifiable {
     var coordinate: CLLocationCoordinate2D
 }
 
-
 struct DetailView: View {
-    
-    
     
     var picture:String
     var cityName:String
     var description:String
+   // var location:CLLocation
     
-    @State private static var defaultLocation = CLLocationCoordinate2D(
+    private static let defaultLocation = CLLocationCoordinate2D(
         latitude: 33.4255,
         longitude: -111.9400
     )
     
-    
-    // state property that represents the current map region
     @State private var region = MKCoordinateRegion(
         center: defaultLocation,
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
-    // state property that stores marker locations in current map region
+        )
+    
     @State private var markers = [
-        Location(name: "", coordinate: defaultLocation)
+        Location(name: "cityName", coordinate: defaultLocation)
     ]
     
     
+    @State var location: CLLocationCoordinate2D?
+
+    
+    
     var body: some View {
-        NavigationView(){
-            
-            Map(coordinateRegion: $region,
-                interactionModes: .all,
-                annotationItems: markers
-            ){ location in
-                MapAnnotation(coordinate: location.coordinate){
-                    Text(cityName).font(.system(size: 36))
+        ZStack(alignment: .bottom)
+        {
+            Map(coordinateRegion: $region, interactionModes: .all, annotationItems: markers) {
+                location in MapAnnotation(coordinate: location.coordinate)
+                {
+                    Text("\(cityName)")
+                    Text(String(location.coordinate.latitude) + "," + String(location.coordinate.longitude))
                 }
                 
-                
             }
+        }            .onAppear{self.getLocation(from: cityName)
+            {coordinates in
+                print(coordinates)
+                self.location = coordinates}
             
-            Button{
-                forwardGeocoding(addressStr: cityName)
-                
-            }label: {
-                Text("Get Location Information")
-            }
-            
-            
-        }.onAppear{self.forwardGeocoding(addressStr: self.cityName)}
+        }
+        
+    
+
     }
     
-    func forwardGeocoding(addressStr: String)
-    {
-        let geoCoder = CLGeocoder();
-        let addressString = addressStr
+    func getLocation(from address: String, completion: @escaping (_ location:CLLocationCoordinate2D?) -> Void) {
+        let geocoder = CLGeocoder();
         
-        var longitude = 0.0;
-
-
-
-        CLGeocoder().geocodeAddressString(addressString, completionHandler:
-                                            {(placemarks, error) in
+        geocoder.geocodeAddressString(address) { (placemarks, error) in guard let placemarks = placemarks,
             
-            if error != nil {
-                print("Geocode failed: \(error!.localizedDescription)")
-            } else if placemarks!.count > 0 {
-                
-                let placemark = placemarks![0]
-                let location = placemark.location
-                let coords = location!.coordinate
-                
-                
-                print(coords.latitude)
-                print(coords.longitude)
-                
-                longitude = coords.longitude
-                
-                
-                
-                
-                DispatchQueue.main.async
-                {
-                    region.center = coords
-                    markers[0].name = placemark.locality!
-                    markers[0].coordinate = coords
-                }
-                
-                
-                
-                
-                
-                
-                
+            let location = placemarks.first?.location?.coordinate
+            else {
+                completion(nil);
+                return
             }
-        })
-            }
+
+                    region.center = location
+            markers[0] = Location(name: cityName, coordinate: location);
+
+
+        }
+    }
+    
+    
+    
+    
+    
     
     
     
